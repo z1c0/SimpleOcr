d462b60a45b7e3eaea26733a0a67b072da5a408e
z1c0 <wziegler@live.at>
2015-04-26 20:25:53 +0200

    Finished demo application for DotNeuralNet

--- SimpleOcr/SimpleOcr.Shared/ViewModels/CellViewModel.cs
+++ SimpleOcr/SimpleOcr.Shared/ViewModels/CellViewModel.cs
@@ -4,5 +4,30 @@ namespace SimpleOcr.ViewModels
 {
   public class CellViewModel : SimpleViewModelBase
   {
+    private bool _isChecked;
+
+    public CellViewModel()
+    {
+      TapCommand = new ActionCommand(_ => IsChecked = true);
+    }
+
+    public override string ToString()
+    {
+      return IsChecked.ToString();
+    }
+
+    public bool IsChecked
+    {
+      get
+      {
+        return _isChecked;
+      }
+      set
+      {
+        SetProperty(ref _isChecked, value);
+      }
+    }
+
+    public ActionCommand TapCommand { get; private set; }
   }
 }
--- SimpleOcr/SimpleOcr.Shared/ViewModels/MainViewModel.cs
+++ SimpleOcr/SimpleOcr.Shared/ViewModels/MainViewModel.cs
@@ -1,25 +1,127 @@
-﻿using Newport;
-using System;
+﻿using System;
 using System.Linq;
 using System.Collections.Generic;
-using System.Text;
+using Newport;
+using DotNeuralNet;
+using System.Threading.Tasks;
 
 namespace SimpleOcr.ViewModels
 {
   [ExportedViewModel]
   public class MainViewModel : ViewModelBase
   {
+    private Network _network;
+    private List<BackPropagationTrainingRow> _trainingRows;
+    private int _selectedNumber;
+    private bool _isTraining;
+
     public MainViewModel()
     {
+      InitNeuralNetwork();
+
       Text = "Simple OCR";
       Rows = Cols = 10;
+      IsTraining = true;
+
       Cells = Enumerable.Range(0, Rows * Cols).Select(_ => new CellViewModel()).ToList();
+      Numbers = Enumerable.Range(0, 10).ToList();
+
+      ResetCommmand = new ActionCommand(_ => Reset());
+      OkCommmand = new ActionCommand(_ =>
+      {
+        _network.Invalidate();
+        if (IsTraining)
+        {
+          var row = BackPropagationTrainingRow.Create(_network);
+          Cells.ForEach((i, c) => row.Inputs[i] = c.IsChecked ? 1 : 0);
+          for (var i = 0; i < 10; i++)
+          {
+            row.Outputs[i] = (SelectedNumber == i) ? 1.0 : 0.0;
+          }
+          _trainingRows.Add(row);
+        }
+        else
+        {
+          // Feed input data to neural network.
+          Cells.ForEach((i, c) => _network.InputNodes[i++].Value = c.IsChecked ? 1 : 0);
+          // Find node with highest activation.
+          var node = _network.OutputNodes.First(n => n.Value == _network.OutputNodes.Max(nn => nn.Value));
+          SelectedNumber = _network.OutputNodes.IndexOf(node);
+        }
+        Reset();
+      });
+    }
+
+    private async void InitNeuralNetwork()
+    {
+      _trainingRows = new List<BackPropagationTrainingRow>();
+      _network = new Network(100, 60, 10);
+      var weights = await IsolatedStorageHelper.Load<double[]>("weights.xml");
+      if (weights != null)
+      {
+        _network.SetWeights(weights);
+      }
     }
 
     public int Cols { get; set; }
 
     public int Rows { get; set; }
 
+    public bool IsTraining
+    {
+      get
+      {
+        return _isTraining;
+      }
+      set
+      {
+        if (!value)
+        {
+          Train();
+        }
+        SetProperty(ref _isTraining, value);
+      }
+    }
+
+    private async void Train()
+    {
+      if (_trainingRows.Count > 0)
+      {
+        using (BusyScope())
+        {
+          await Task.Factory.StartNew(() =>
+          {
+            var trainer = new BackPropagationTrainer(_network);
+            trainer.Train(_trainingRows, 0.5, 50);
+            IsolatedStorageHelper.Save("weights.xml", _network.GetWeights());
+          });
+        }
+      }
+    }
+
     public IEnumerable<CellViewModel> Cells { get; private set; }
+
+    public ActionCommand ResetCommmand { get; private set; }
+
+    public ActionCommand OkCommmand { get; private set; }
+
+    public List<int> Numbers { get; private set; }
+
+    public int SelectedNumber
+    {
+      get
+      {
+        return _selectedNumber;
+      }
+      set
+      {
+        SetProperty(ref _selectedNumber, value);
+      }
+    }
+
+    private void Reset()
+    {
+      Cells.ForEach(c => c.IsChecked = false);
+    }
   }
 }
